1fdaed74880ac65ccac28ae2ca1e6e32b2e3acd5
Svjatoslav Torn <sefkiss.torn@yandex.ru>
2021-07-11 07:38:25 +0500

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // Получим ширину экрана устройства
+    double widthScreen = MediaQuery.of(context).size.width;
+
     return DefaultTabController(
       length: nav.length,
       child: Scaffold(
@@ -73,7 +76,9 @@ class _MyHomePageState extends State<MyHomePage> {
             return ListView(
               key: PageStorageKey(tabName),
               children: <Widget>[
-                for (var item in data[tabName]) PhotoCard(urlImage: item),
+                // Дополнительно передаем в Карточку реальную ширину экрана
+                for (var item in data[tabName])
+                  PhotoCard(urlImage: item, widthScreen: widthScreen),
               ],
             );
           }).toList(),
@@ -85,30 +90,55 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class PhotoCard extends StatelessWidget {
   final String urlImage;
+  // Обещаем что позже эта переменная не будет NULL
+  late Map<String, double> sizeImage;
+  double widthScreen;
+  // Дефолтная заглушка если null
   static const String nullUrl = 'https://picsum.photos/200/200';
 
-  const PhotoCard({Key? key, this.urlImage = nullUrl}) : super(key: key);
+  PhotoCard({Key? key, this.urlImage = nullUrl, this.widthScreen = 300})
+      : super(key: key) {
+    // На этапе конструктора получаем ширину и высоту для плэйсхолдера
+    sizeImage = getSizeImage(urlImage, widthScreen);
+  }
+
+  // Функция для получения идеально точного плэйсхолдера из ссылки
+  Map<String, double> getSizeImage(String url, double widthScreen) {
+    // Разбиваем ссылку
+    List<String> list = url.split('/');
+    // Знаем что в конце высота, берем ее
+    double height = double.parse(list.last);
+    // Удаляем высоту из массива
+    list.removeLast();
+    // Теперь последний элемент это ширина
+    double width = double.parse(list.last);
+    // Считаем коэффициент уменьшения, ширину картинки делим на реальную ширину экрана устройства
+    double ratio = width / widthScreen;
+    // Возвращаем массив данных
+    return <String, double>{
+      'height': height / ratio,
+      'width': width / ratio,
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.symmetric(vertical: 5),
         child: Image.network(
           urlImage,
           frameBuilder: (BuildContext context, Widget child, int? frame,
               bool wasSynchronouslyLoaded) {
             if (wasSynchronouslyLoaded) return child;
-            return AnimatedOpacity(
-                opacity: frame == null ? 0 : 1,
-                duration: const Duration(milliseconds: 500),
-                child: child,
-                curve: Curves.easeInOut);
-          },
-          loadingBuilder: (BuildContext context, Widget child,
-              ImageChunkEvent? loadingProgress) {
-            if (loadingProgress == null) return child;
-            return const CircularProgressIndicator.adaptive();
+            return Container(
+              height: sizeImage['height'],
+              width: sizeImage['width'],
+              color: Colors.black26,
+              child: const Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            );
           },
         ),
       ),
